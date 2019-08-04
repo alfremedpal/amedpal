27bf5ea081f9cd327ca4d64c633342ac40b03efc
alfremedpal <alfremedpal@gmail.com>
2019-08-04 11:22:34 -0500

    Release 1

--- amedpal/index/routes.py
+++ amedpal/index/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import render_template, abort
+from jinja2 import exceptions
 
 from . import index
 
 @index.route("/")
 def home():
-    return "Hello, amedpal!"
+    return render_template("index.html")
+
+@index.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@index.route("/blog/<post>")
+def blog_post(post):
+    try:
+        return render_template("posts/" + post + ".html")
+    except exceptions.TemplateNotFound:
+        abort(404)
--- run.py
+++ run.py
@@ -9,3 +9,4 @@ app.app_context().push()
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
